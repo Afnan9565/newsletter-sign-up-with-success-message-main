2b5a1b1ed1b88527a0b84acec08039055a8c66e6
Afnan Ahmad <afnanahmad9565@gmail.com>
2023-10-31 18:06:16 +0530

    Logic Completed

--- script.js
+++ script.js
@@ -8,13 +8,16 @@ const emailInput = document.querySelector("#email");
 const userEmailEl = document.querySelector("#user-email");
 const submitBtnEl = document.querySelector("#submit");
 
+submitBtnEl.addEventListener("click", function () {
+  reaction.classList.remove("hide");
+  action.classList.add("hide");
+  const email = emailInput.value;
+  userEmailEl.innerText = email;
+   
+  console.log("submitted");
+});
 dismiss.addEventListener("click", function () {
   reaction.classList.add("hide");
   action.classList.remove("hide");
 });
 // .show = .active
-submitBtnEl.addEventListener("click", function () {
-   reaction.classList.remove("hide");
-   action.classList.add("hide");
- });
- 
