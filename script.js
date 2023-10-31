edbc9c9049a5a1df1edf8e56ce657f7bb878f37e
Afnan Ahmad <afnanahmad9565@gmail.com>
2023-10-31 13:03:52 +0530

    Javascript added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Defining variables
+const submit = document.querySelector("#submit");
+const dismiss = document.querySelector("#dismiss");
+const reaction = document.querySelector(".reaction");
+const action = document.querySelector("#action");
+
+dismiss.addEventListener('click', function() {
+   reaction.classList.add("hide");
+   action.classList.remove("hide")
+});
