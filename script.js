c067dc5239d9c26369a1bb210ee64ae52032a826
Afnan Ahmad <afnanahmad9565@gmail.com>
2023-10-31 17:47:08 +0530

    Logic added

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Defining variables
-const submit = document.querySelector("#submit");
+// const submit = document.querySelector("#submit");
 const dismiss = document.querySelector("#dismiss");
 const reaction = document.querySelector(".reaction");
 const action = document.querySelector("#action");
+const invalidEmailEl = document.querySelector("#invalid-email");
+const emailInput = document.querySelector("#email");
+const userEmailEl = document.querySelector("#user-email");
+const submitBtnEl = document.querySelector("#submit");
 
-dismiss.addEventListener('click', function() {
-   reaction.classList.add("hide");
-   action.classList.remove("hide")
+dismiss.addEventListener("click", function () {
+  reaction.classList.add("hide");
+  action.classList.remove("hide");
 });
+// .show = .active
+submitBtnEl.addEventListener("click", function () {
+   reaction.classList.remove("hide");
+   action.classList.add("hide");
+ });
+ 
